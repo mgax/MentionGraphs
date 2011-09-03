72bb0fbf5a64c5ecb88b1fa8ceaf6dbaaaedb374
Alex Bardas <alexbardas@gmail.com>
2011-09-03 14:23:04 +0300

    template

--- MentionGraphs/frontend/views.py
+++ MentionGraphs/frontend/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.decorators.http import require_POST, require_GET
 
+@require_GET
 def index(request):
-    return render_to_response('index.html', {})
+    context = {}
+    return render_to_response('index.html',
+     context, context_instance=RequestContext(request))
+
 def api(request):
     return HttpResponse('Hello World')
