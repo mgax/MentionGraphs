bbc5dca53f8e3b85700f8f2acddd8aae67380829
Alex Morega <alex@grep.ro>
2011-09-03 15:51:45 +0300

    test for one api call

---
+++ MentionGraphs/firehose/crawl.py
@@ -0,0 +1,21 @@
+import urllib
+import json
+
+API_KEY = 'asdf'
+FIELDS = ['language', 'generator', 'sentiment', 'published']
+
+
+def do_api_call(keyword, generator):
+    base = 'http://api.contextvoice.com/1.2/mentions/search/mongo/?'
+    url = base + urllib.urlencode({
+        'q': keyword,
+        'generator': generator,
+        'apikey': API_KEY,
+    })
+
+    response = urllib.urlopen(url)
+    data = json.load(response)
+    response.close()
+
+    for mention in data['results']:
+        yield {k: mention.get(k, None) for k in FIELDS}
--- MentionGraphs/firehose/tests.py
+++ MentionGraphs/firehose/tests.py
@@ -1,16 +1,39 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from StringIO import StringIO
+import json
 from django.test import TestCase
+from mock import patch
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ApiCallTest(TestCase):
+
+    def setUp(self):
+        import urllib
+        self._urllib_mock = patch('MentionGraphs.firehose.crawl.urllib')
+        mock_urllib = self._urllib_mock.start()
+        self.mock_urlopen = mock_urllib.urlopen
+        mock_urllib.urlencode = urllib.urlencode
+
+    def tearDown(self):
+        self._urllib_mock.stop()
+
+    def set_api_response(self, mentions):
+        data = {
+            'count': len(mentions),
+            'total': len(mentions),
+            'results': mentions,
+        }
+        self.mock_urlopen.return_value = StringIO(json.dumps(data))
+
+    def test_api_call(self):
+        from crawl import do_api_call
+        item1 = {
+            'language': 'english',
+            'generator': 'twitter',
+            'sentiment': 'neutral',
+            'published': 1315051701,
+        }
+        self.set_api_response([item1])
+
+        mentions = list(do_api_call('python', 'twitter'))
+
+        self.assertEqual(mentions, [item1])
