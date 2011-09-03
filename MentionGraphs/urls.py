f1519a0e8bcd1b729f2d79ae644c6b04234e8754
Alex Morega <alex@grep.ro>
2011-09-03 14:22:13 +0300

    django admin for the Datapoint model

--- MentionGraphs/urls.py
+++ MentionGraphs/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+import MentionGraphs.frontend.admin
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,5 +18,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
