3172c0cbcec96ca7bbe7f17d16264b78fca123b5
Alex Bardas <alexbardas@gmail.com>
2011-09-04 15:13:23 +0300

    update

--- MentionGraphs/frontend/views.py
+++ MentionGraphs/frontend/views.py
@@ -4,6 +4,19 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.http import require_POST, require_GET
 from filter_names import filters, streams
+from MentionGraphs.firehose.models import Datapoint, Keyword, Metric
+import datetime
+import json
+import time
+
+from django import template
+register = template.Library()
+@register.filter
+def epoch(value):
+    try:
+        return int(time.mktime(value.timetuple())*1000)
+    except AttributeError:
+        return ''
 
 @require_GET
 def index(request):
@@ -15,6 +28,21 @@ def index(request):
 
 @require_GET
 def api(request):
-    for k, v in request.GET.items():
-        print k, v
-    return HttpResponse(stream)
+    for k,v in request.GET.items():
+        if k == 'stream':
+            q = v
+        else:
+            metric_name = k
+            metric_value = v
+
+    keyword_model = Keyword.objects.filter(name=q)
+    metric_model = Metric.objects.filter(name=metric_name, value=metric_value)
+    dataset = Datapoint.objects.filter(keyword=keyword_model, metric=metric_model)
+
+    response = []
+    for item in dataset:
+       entry={}
+       entry[epoch(item.time)]=item.count
+       response.append(entry)
+    return HttpResponse(json.dumps(response), mimetype="application/json")
+
