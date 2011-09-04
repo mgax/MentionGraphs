38a0c664af4ecfbb8cd3819516d764aea8b55249
Alex Morega <alex@grep.ro>
2011-09-04 13:48:05 +0300

    friendly representation of model instances

--- MentionGraphs/firehose/models.py
+++ MentionGraphs/firehose/models.py
@@ -1,13 +1,21 @@
+# encoding: utf-8
+
 from django.db import models
 
 
 class Keyword(models.Model):
     name = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Metric(models.Model):
     name = models.CharField(max_length=255)
     value = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return "%s:%s" % (self.name, self.value)
 
 
 class Datapoint(models.Model):
@@ -15,6 +23,10 @@ class Datapoint(models.Model):
     time = models.DateTimeField()
     metric = models.ForeignKey(Metric)
     count = models.IntegerField()
+
+    def __unicode__(self):
+        return u"%s, %s, %s « %d »" % (self.keyword, self.time,
+                                   self.metric, self.count)
 
 def save_data(keyword, data):
     k = Keyword.objects.get_or_create(name=keyword)[0]
